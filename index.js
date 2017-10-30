fc37f7f623966973420a925fb67af499d94ae9cf
Rafael Jácome <rafaeljacome@Rafaels-MacBook-Pro.local>
2017-10-30 12:58:07 -0600

    updated

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/css/',function(req,res){
+  res.sendFile(path.join(__dirname+'/dist/css/style.min.css'));
+});
+
+app.get('/js/',function(req,res){
+  res.sendFile(path.join(__dirname+'/dist/js/script.min.js'));
+});
+
 app.use(express.static('dist'));
 
 var port = process.env.PORT || 3000;
