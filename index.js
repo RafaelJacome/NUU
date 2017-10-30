93c5d62e0f914d57e91e12364a79fb525f466ffa
Rafael Jácome <rafaeljacome@Rafaels-MacBook-Pro.local>
2017-10-30 12:31:10 -0600

    updated

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ app.get('/',function(req,res){
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+var port = process.env.PORT || 3000;
+app.listen(port);
